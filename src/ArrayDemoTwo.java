0d471488131802b61ed19cc7ce7931b8d3bf7fa1
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-07-03 12:52:54 -0400

    Programs that prints out elements of an array

--- src/ArrayDemoTwo.java
+++ src/ArrayDemoTwo.java
@@ -1,3 +1,12 @@
-public class Main {
+public class ArrayDemoTwo {
+    public static void main(String[] args) {
 
+        int[] age = {12, 4, 5};
+
+        System.out.println("Using for Loop: ");
+
+        for(int i = 0; i < age.length; i++) {
+            System.out.println(age[i]);
+        }
+    }
 }
