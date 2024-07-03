c834f00211180d4f6abb023833c3d1daba6f0d51
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-07-03 12:52:08 -0400

    Programs that access elements of an array

--- src/ArrayDemoOne.java
+++ src/ArrayDemoOne.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ArrayDemoOne {
+public class ArrayDemoOne {
+    public static void main(String[] args) {
+
+        int[] age = {12, 4, 5, 2, 5};
+
+        System.out.println("Accesing Elements of an Array: ");
+        System.out.println("First Element: "  + age[0]);
+        System.out.println("Second Element: "+ age[1]);
+        System.out.println("Third Element: " + age[2]);
+        System.out.println("Fourth Element: " + age[3]);
+        System.out.println("Fifth Element: " +age[4]);
+
+    }
 }
